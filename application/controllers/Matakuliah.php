89ff5b7ee230dda6c10fe83352208655541ca33c
Eko-Saputro <eko.saputro.come@gmail.com>
2023-10-17 21:14:05 +0700

    Pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -6,7 +6,38 @@ class Matakuliah extends CI_Controller{
     }
   public function cetak()
     {
-      $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required');
+      $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'trim|required|numeric|min_length[3]',
+      array('required' => '%s Wajib diisi.',
+            'numeric' => '%s Wajib diisi angka.',
+            'min_length' => '%s 3 Karakter.'));
+      $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|alpha',
+      array('required' => '%s Wajib diisi.',
+            'alpha' => '%s Wajib diisi huruf.'
+            ));
+      $this->form_validation->set_rules('sks', 'SKS', 'required',
+      array('required' => '%s Wajib diisi.'
+            ));
+
+      if ($this->form_validation->run() == FALSE)
+                      {
+                              $this->load->view('view-form-matakuliah');
+                      }
+                      else
+                      {
+                        $data = [
+                                  'kode' => $this->input->post('kode'),
+                                  'nama' => $this->input->post('nama'),
+                                  'sks' => $this->input->post('sks')
+                                          ];
+
+                          $this->load->view('view-data-matakuliah', $data);
+                      }
+
+
+
+
+
+    /*  $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required');
       $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required');
       $this->form_validation->set_rules('sks', 'SKS', 'required');
 
@@ -23,14 +54,8 @@ class Matakuliah extends CI_Controller{
                                     ];
 
                     $this->load->view('view-data-matakuliah', $data);
-                }
+                }*/
 
-/*$data = [
-          'kode' => $this->input->post('kode'),
-          'nama' => $this->input->post('nama'),
-          'sks' => $this->input->post('sks')
-                  ];
 
-  $this->load->view('view-data-matakuliah', $data);*/
 }
 }
--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title></title>
   </head>
-  <?php echo validation_errors(); ?>
   <body>
+    
 
     <center>
       <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
@@ -24,15 +24,17 @@
                    <th>Kode MTK</th>
                    <td>:</td>
                    <td>
-                       <input type="text" name="kode" id="kode">
+                       <input type="text" name="kode" id="kode" value="<?php echo set_value('kode'); ?>">
                    </td>
+                   <td><?php echo form_error('kode'); ?></td>
                </tr>
                <tr>
                    <th>Nama MTK</th>
                    <td>:</td>
                    <td>
-                       <input type="text" name="nama" id="nama" >
+                       <input type="text" name="nama" id="nama" value="<?php echo set_value('nama'); ?>">
                    </td>
+                   <td><?php echo form_error('nama'); ?></td>
                </tr>
                <tr>
                    <th>SKS</th>
@@ -45,6 +47,7 @@
                            <option value="4">4</option>
                        </select>
                    </td>
+                   <td><?php echo form_error('sks'); ?></td>
                </tr>
                <tr>
                    <td colspan="3" align="center">
