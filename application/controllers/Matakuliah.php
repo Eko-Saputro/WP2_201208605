2e9cf952c64a37d5e5723ce762e1f144b34ff9ef
Eko-Saputro <eko.saputro.come@gmail.com>
2023-10-17 19:17:32 +0700

    Pertemuan 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -6,13 +6,31 @@ class Matakuliah extends CI_Controller{
     }
   public function cetak()
     {
+      $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required');
+      $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required');
+      $this->form_validation->set_rules('sks', 'SKS', 'required');
 
-$data = [
+      if ($this->form_validation->run() == FALSE)
+                {
+                        $this->load->view('view-form-matakuliah');
+                }
+                else
+                {
+                  $data = [
+                            'kode' => $this->input->post('kode'),
+                            'nama' => $this->input->post('nama'),
+                            'sks' => $this->input->post('sks')
+                                    ];
+
+                    $this->load->view('view-data-matakuliah', $data);
+                }
+
+/*$data = [
           'kode' => $this->input->post('kode'),
           'nama' => $this->input->post('nama'),
           'sks' => $this->input->post('sks')
                   ];
 
-  $this->load->view('view-data-matakuliah', $data);
+  $this->load->view('view-data-matakuliah', $data);*/
 }
 }
